2cfc1d6c6f908903092122e4c2c43e7e4108ae21
Ahshan Habib <ahshanhabib1216@gmail.com>
2023-10-25 14:33:57 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@ using namespace std; // Make the std namespace the default
 
 int main() {
     string inputString = "hexlloXwXorld";
-    size_t xPos = inputString.find('X');
+    size_t xPos = inputString.find_first_of("Xx");
     size_t lastPos = 0;
 //cout<<xPos; xpos=7 , lastpos 6
     while (xPos != string::npos) {
+        if (inputString[xPos] == 'X' || inputString[xPos] == 'x')
+        {
         cout << inputString.substr(lastPos, xPos - lastPos) << endl;
+        }
         lastPos = xPos + 1;
-        xPos = inputString.find('X', lastPos);
+        xPos = inputString.find_first_of("Xx", lastPos);
     }
 
     cout << inputString.substr(lastPos) << endl;
@@ -19,4 +22,3 @@ int main() {
 
     return 0;
 }
-
